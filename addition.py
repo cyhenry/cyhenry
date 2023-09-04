c9d68e719e6cdc32641911aafbf13d5bf8abc36a
cyhenry <144000237+cyhenry@users.noreply.github.com>
2023-09-04 15:40:24 +0800

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,13 @@
+# Replace ___ with your code
+
+# create variable number1
+number1 = 
+
+# create variable number2
+number2 = 
+
+# add number1 and number2
+result = number1 ___ number2
+
+# print result
+print(result)
